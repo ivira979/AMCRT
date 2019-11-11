a1caa5d550441083958dd708ee3a94776a66cb59
ivira979 <ivira979@mtroyal.ca>
2019-11-11 00:35:40 -0700

    updated sh

--- misc/emailcleaner.sh
+++ misc/emailcleaner.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find /home/vpe/mail/amcrt.ca/accounts/new -mtime +30 -type f -ls -delete
-find /home/vpe/mail/amcrt.ca/accounts/cur -mtime +30 -type f -ls -delete
+find /home/r3hx0olc5t2w/mail/amcrt.ca/vpe/new -mtime +30 -type f -ls -delete
+find /home/r3hx0olc5t2w/mail/amcrt.ca/vpe/cur -mtime +30 -type f -ls -delete
